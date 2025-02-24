b19a0f8da98d241fddbef898297325fdce716ad4
Prashik17 <moonprashik17@gmail.com>
2025-02-24 12:59:54 +0530

    Blank Project

---
+++ src/test/java/com/vehicleinsurance/Vehicle/Insurance/VehicleInsuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vehicleinsurance.Vehicle.Insurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleInsuranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
